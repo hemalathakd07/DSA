6056c0ad688cbb2ea33d7ed7a193fafd8924d27e
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-27 21:31:51 +0530

    Handled array full edge case during insertion

--- Arrays/InsertionAtBeginning.java
+++ Arrays/InsertionAtBeginning.java
@@ -1,29 +1,33 @@
-import java.util.Scanner;
+  import java.util.Scanner;
 
-public class InsertionAtBeginning{
-  static void insertionAtBeginning(int[] arr,int size,int element){
-    for(int i=size;i>0;i--){
-      arr[i]=arr[i-1];
+  public class InsertionAtBeginning{
+    static void insertionAtBeginning(int[] arr,int size,int element){
+      for(int i=size;i>0;i--){
+        arr[i]=arr[i-1];
+      }
+      arr[0]=element;
+
     }
-    arr[0]=element;
+    public static void main(String[] args) {
+      int[] array=new int[20];
+      int size=5;
+      int element=0;
+      Scanner scanner=new Scanner(System.in);
+      System.out.println("Enter array elements:");
+      for(int i=0;i<size;i++){
+        array[i]=scanner.nextInt();
 
+      }
+      if(size<array.length){
+      insertionAtBeginning(array, size, element);
+      size++;
+    }else{
+      System.out.println("Array is full! Cannot be inserted");
+    }
+      System.out.println("Array is :");
+      for(int i=0;i<size;i++){
+        System.out.print(array[i]+"\t");
+      }
+      scanner.close();
+    }
   }
-  public static void main(String[] args) {
-    int[] array=new int[20];
-    int size=5;
-    int element=0;
-    Scanner scanner=new Scanner(System.in);
-    System.out.println("Enter array elements:");
-    for(int i=0;i<size;i++){
-      array[i]=scanner.nextInt();
-
-    }
-    insertionAtBeginning(array, size, element);
-    size++;
-    System.out.println("Array is :");
-    for(int i=0;i<size;i++){
-      System.out.print(array[i]+"\t");
-    }
-    scanner.close();
-  }
-}
