d615cff11750ff6912235f0ba6d8eafc9002b635
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-29 05:49:43 +0530

    Insert an element at the beginning of the array using new array

---
+++ Arrays/InsertionAtBeginningUsingNewArray.java
@@ -0,0 +1,21 @@
+public class InsertionAtBeginningUsingNewArray {
+  void insertion(int[] array,int[] newArray,int element){
+    newArray[0]=element;
+    for(int i=0;i<array.length;i++){
+      newArray[i+1]=array[i];
+    }
+  }
+
+  public static void main(String[] args) {
+    int[] array={1,2,3,4,5};
+    int[] newArray=new int[array.length+1];
+    int element=0;
+    InsertionAtBeginningUsingNewArray obj=new InsertionAtBeginningUsingNewArray();
+    obj.insertion(array,newArray,element);
+
+  for(int i:newArray){
+    System.out.print(i+"\t");
+  }
+  
+  }
+}
