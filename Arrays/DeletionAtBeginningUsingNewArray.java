dbd510b47945191ee06e3dafd0ec0a564a16092e
hemalathakd07 <hemalathakd07@gmail.com>
2026-04-30 04:38:32 +0530

    Add a program to delete an element at the beginning of the array using new array

---
+++ Arrays/DeletionAtBeginningUsingNewArray.java
@@ -0,0 +1,27 @@
+public class DeletionAtBeginningUsingNewArray {
+  void deletion(int[] array,int[] newArray){
+    for(int i=0;i<newArray.length;i++){
+      newArray[i]=array[i+1];
+
+    }
+  }
+  void traversal(int[] array){
+    for(int i=0;i<array.length;i++){
+      System.out.print(array[i]+"\t");
+    }
+  }
+  public static void main(String[] args) {
+    int[] array={0,1,2,3,4,5};
+    int[] newArray=new int[array.length-1];
+    System.out.println("Array elements before deletion:");
+    DeletionAtBeginningUsingNewArray obj=new DeletionAtBeginningUsingNewArray();
+    obj.traversal(array);
+
+    obj.deletion(array,newArray);
+    System.out.println("\nArray elements after deletion is ");
+    obj.traversal(newArray);
+    
+    
+  }
+  
+}
