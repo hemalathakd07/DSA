6ea902343b8d1e450009769538ecc6848973b290
hemalathakd07 <hemalathakd07@gmail.com>
2026-05-13 19:27:42 +0530

    Add a programn to find the largest element in the given array

---
+++ Arrays/A2Z/largestElement.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+
+public class largestElement {
+  static int largest(int[] array){
+    int largest_element=array[0];
+    for(int i=0;i<array.length;i++){
+      if(array[i]>largest_element){
+        largest_element=array[i];
+      }
+    }
+    return largest_element;
+  }
+  public static void main(String[] args) {
+    int[] array={2,45,1,7,10};
+    System.out.println(largest(array));
+  }
+  
+}
